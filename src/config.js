baaff0236570abe281aa83147f70bd65af58c523
saguins <saralara555@gmail.com>
2018-06-25 21:43:41 +0700

    All my code

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  gameWidth: 400,
+  gameHeight: 600,
+  localStorageName: 'phaseres6webpack',
+  webfonts: ['Bangers']
+}
---
+++ src/sprites/Bar.js
@@ -0,0 +1,7 @@
+class Bar extends Phaser.Sprite {
+    constructor({ game, x, y, asset }) {
+        super(game, x, y, asset)
+    }
+}
+
+export default Bar;
